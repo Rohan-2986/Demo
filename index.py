d88395fedf162a6633c63217ab818934762009bc
Rohan Patil <rohanpatil2986@gmail.com>
2026-07-27 18:41:51 +0530

    new branch new feature added

--- index.py
+++ index.py
@@ -1,2 +1,3 @@
 print("Hello World!")
-print("git init \ngit status \ngit add . \n git commit -m 'message'")
+print("git init \ngit status \ngit add . \n git commit -m 'message' ")
+print("git branch 'BranchName' \ngit checkout 'BranchName' \nthen follow the process of add and commit changes")
