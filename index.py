28056c01d3afe901f1e4145e4803e04666aaedc5
Rohan Patil <rohanpatil2986@gmail.com>
2026-07-28 06:41:16 +0530

    delete index.py file

--- index.py
+++
@@ -1,4 +0,0 @@
-print("Hello World!")
-print("git init \ngit status \ngit add . \n git commit -m 'message' ")
-print("git branch 'BranchName' \ngit checkout 'BranchName' \nthen follow the process of add and commit changes")
-print("for seeing an version or old commits use following command \n git log \n git log --all \n git log --all --graph")
